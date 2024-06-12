7c9a1b1a270b1d3669c4c4e1a3861de10caba1b1
tl-pp <571170725@qq.com>
2024-06-12 16:40:10 +0800

    feat: add image threshold test;

--- image_threshold.py
+++ image_threshold.py
@@ -79,7 +79,69 @@ def ImageGlobalThreshold(filepath1):
     plt.show()
 
 
+def ImageOTSU(filepath1):
+    img = cv.imread(filepath1, flags=0)
 
+    # OTSU 算法实现
+    histCV = cv.calcHist([img], [0], None, [256], [0, 256])   # 灰度直方图
+    scale = range(256)   # 灰度级[0, 255]
+    totalPixels = img.shape[0] * img.shape[1]   # 像素总数
+    totalGray = np.dot(histCV[:, 0], scale)   # 内积， 像素总和
+    mG = totalGray / totalPixels  # 平均灰度
+    icv = np.zeros(256)
+    numFt, sumFt = 0, 0
+
+    for t in range(256):
+        numFt += histCV[t, 0]       # F(t)像素数量
+        sumFt += histCV[t, 0] * t   # F(t)的灰度值总和
+        pF = numFt / totalPixels    # F(t)的像素数占比
+        mF = (sumFt / numFt) if numFt > 0 else 0    # F(t)的平均灰度
+        numBt = totalPixels - numFt  # B(t)像素数量
+        sumBt = totalGray - numFt    # B(t)的灰度值总和
+        pB = numBt / totalPixels     # B(t)像素数占比
+        mB = (sumBt / numBt) if numBt > 0 else 0    # B(t)的平均灰度
+        icv[t] = pF * (mF - mG) ** 2 + pB * (mB - mG) ** 2   # 灰度t的类间方差
+    
+    maxIcv = max(icv)   # ICV的最大值
+    maxIndex = np.argmax(icv)  # 最大值的索引， 即OTSU阈值
+
+    _, imgBin = cv.threshold(img, maxIndex, 255, cv.THRESH_BINARY)   # 以maxIndex为最优阈值
+
+    # 函数cv.threshold 实现OTSU算法      
+    ret, imgOTSU = cv.threshold(img, 128, 255, cv.THRESH_OTSU)   # OTSU 阈值分割
+    print("maxIndex = {}, retOTSU = {}".format(maxIndex, round(ret)))
+    plt.figure(figsize=(9,3.5))
+    plt.subplot(131),plt.axis('off'),plt.title("1. Original"),plt.imshow(img, cmap='gray')
+    plt.subplot(132),plt.axis('off'),plt.title("2. OTSU by ICV (T={})".format(maxIndex)),plt.imshow(imgBin, cmap='gray')
+    plt.subplot(133),plt.axis('off'),plt.title("3. OTSU by OPENCV (T={})".format(round(ret))),plt.imshow(imgOTSU, cmap='gray')
+    plt.tight_layout()
+    plt.show()
+
+
+def ImageAdaptiveThreshold(filepath1):
+    img = cv.imread(filepath1, flags=0)
+
+    # 自适应局部阈值处理
+    binaryMean = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_MEAN_C, cv.THRESH_BINARY, 3, 3)
+
+    binaryGauss = cv.adaptiveThreshold(img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 5, 3)
+
+    # 参考方法 ： 自适应局部阈值处理
+    ratio = 0.03
+    imgBlur = cv.boxFilter(img, -1, (3, 3))  # 盒式滤波器， 均值平滑
+    localThresh = img - (1.0 - ratio) * imgBlur
+    binaryBox = np.ones_like(img) * 255      # 创建与img相同形状的白色图像
+    binaryBox[localThresh < 0] = 0
+
+    plt.figure(figsize=(9,3))
+    plt.subplot(131),plt.axis('off'),plt.title("1. adaptive mean"),plt.imshow(binaryMean, cmap='gray')
+    plt.subplot(132),plt.axis('off'),plt.title("2. adaptive Gauss"),plt.imshow(binaryGauss, cmap='gray')
+    plt.subplot(133),plt.axis('off'),plt.title("3. adaptive local thresh"),plt.imshow(binaryBox, cmap='gray')
+    plt.tight_layout()
+    plt.show()
+
+
+    
 
 
 if __name__ == "__main__" :
@@ -90,4 +152,10 @@ if __name__ == "__main__" :
     # ImageFixedThreshold(filepath1)
 
     # 全局阈值法
-    ImageGlobalThreshold(filepath2)
+    # ImageGlobalThreshold(filepath2)
+
+    # 阈值处理之OTSU
+    # ImageOTSU(filepath2)
+
+    # 自适应阈值处理
+    ImageAdaptiveThreshold(filepath2)
